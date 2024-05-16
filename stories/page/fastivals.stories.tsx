ad1b958748194d77f19670a4ae24b5be02492c93
JUNSEOK <kd02109@gmail.com>
2024-05-16 21:30:46 +0900

    feat : storybook mock 데이터 설정

--- stories/page/fastivals.stories.tsx
+++ stories/page/fastivals.stories.tsx
@@ -1,6 +1,8 @@
+import * as FestivalAPI from '@/app/[locale]/(root-nav)/action';
 import Page from '@/app/[locale]/(root-nav)/festivals/page';
 import RootNavLayout from '@/app/[locale]/(root-nav)/layout';
 import LocaleLayout from '@/app/[locale]/layout';
+import { festivalLinupes } from '@/app/lib/mock/data';
 import { Meta, StoryObj } from '@storybook/react';
 import * as NextIntlServer from 'next-intl/server';
 import { createMock } from 'storybook-addon-module-mock';
@@ -27,7 +29,13 @@ export const Default: Story = {
         getTranslationsMock.mockReturnValue(
           Promise.resolve<any>((link: string) => link),
         );
-        return [getTranslationsMock];
+
+        const heroMock = createMock(FestivalAPI, 'getLineupHero');
+        heroMock.mockReturnValue(Promise.resolve(festivalLinupes));
+
+        const festivalMock = createMock(FestivalAPI, 'getLineupInfos');
+        festivalMock.mockReturnValue(Promise.resolve(festivalLinupes));
+        return [getTranslationsMock, heroMock, festivalMock];
       },
     },
   },
