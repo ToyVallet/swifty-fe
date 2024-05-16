9118420ec7f52b46c2c566a15ec45e2d8e19ed17
JUNSEOK <kd02109@gmail.com>
2024-05-16 19:06:50 +0900

    fix : css 수정, 최대 너비, 배경 색

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -2,7 +2,7 @@ import { PropsWithChildren } from 'react';
 
 export default function LocaleLayout({ children }: PropsWithChildren) {
   return (
-    <div className="max-w-[800px] min-w-[320px] m-auto h-[100dvh] overflow-x-hidden scrollbar-hide relative">
+    <div className="max-w-[430px] min-w-[320px] m-auto h-[100dvh] overflow-x-hidden scrollbar-hide relative bg-bgBlack ">
       {children}
     </div>
   );
