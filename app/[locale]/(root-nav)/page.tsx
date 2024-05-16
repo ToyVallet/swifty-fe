9c01a4c9966fef710c5afc58f1d9adb4464a1e3b
이호연 <ho991217@kakao.com>
2024-05-16 13:46:47 +0900

    fix: MenuTile 오류 수정

--- app/[locale]/(root-nav)/page.tsx
+++ app/[locale]/(root-nav)/page.tsx
@@ -1,19 +1,16 @@
-import { AdBanner, Carousel, Hero, HeroTile } from '@components/common';
 import {
-  LineupTile,
+  AdBanner,
+  Carousel,
+  Hero,
+  HeroTile,
   MenuTiles,
-  NeedReverificationTile,
-} from '@components/home';
+} from '@components/common';
+import { NeedReverificationTile } from '@components/home';
 import { festivalLinupes } from '@lib/mock/data';
 
-import { getAllLineupInfo } from '../(back-nav)/lineup/actions';
-
 export default async function Home() {
-  //const lineups = await getAllLineupInfo();
-
   return (
     <div className="mb-20 flex flex-col gap-4 lg:mx-auto lg:max-w-full lg:gap-8">
-      <NeedReverificationTile />
       <Hero>
         <Carousel hasIndicator>
           {festivalLinupes.map((fetsival) => (
@@ -21,9 +18,6 @@ export default async function Home() {
           ))}
         </Carousel>
       </Hero>
-
-      <AdBanner />
-      <MenuTiles />
     </div>
   );
 }
