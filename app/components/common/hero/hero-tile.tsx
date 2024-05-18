148f3fb6e259a054532c67d022db8c23f6dfc9b7
hyunwlee <hwlee3928@gmail.com>
2024-05-19 02:07:22 +0900

    chore: 불필요한 div태그 수정, tile description width 조정

--- app/components/common/hero/hero-tile.tsx
+++ app/components/common/hero/hero-tile.tsx
@@ -12,7 +12,6 @@ export default function HeroTile({
   enddate,
   description,
   festivalimage,
-  url,
   priority = false,
 }: FestivalInfo & { priority?: boolean }) {
   return (
@@ -33,15 +32,13 @@ export default function HeroTile({
         <div className="absolute bottom-0 left-0 right-0 top-0 bg-gradient-hero-carousel" />
       </div>
 
-      <div className="absolute bottom-[60px] left-5 right-[74px] flex h-[114px] gap-y-1.5 flex-col text-white">
-        <div className="text-xl font-semibold">
-          <span className='mt-[3px] text-xs leading-[18px] tracking-[-0.36px]'>{addr}</span>
-          <h4 className='font-bold text-[22px] leading-[33px] tracking-[-0.66px]'>{name}</h4>
-          <span className='block text-xl leading-[30px] tracking-[-0.6px] truncate'>{description}</span>
-          <span className='mt-[6px] text-sm tracking-[-0.42px]'>
-            {formatDate(startdate, enddate, 'ko')}
-          </span>
-        </div>
+      <div className="absolute bottom-[60px] left-5 right-[74px] h-[114px] text-xl font-semibold">
+        <span className='mt-[3px] text-xs leading-[18px] tracking-[-0.36px]'>{addr}</span>
+        <h4 className='font-bold text-[22px] leading-[33px] tracking-[-0.66px]'>{name}</h4>
+        <span className='block leading-[30px] tracking-[-0.6px] truncate'>{description}</span>
+        <span className='mt-[6px] text-sm tracking-[-0.42px]'>
+          {formatDate(startdate, enddate, 'ko')}
+        </span>
       </div>
     </Link>
   );
